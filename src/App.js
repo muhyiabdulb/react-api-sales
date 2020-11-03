293b1ebf3fd78467f654e46f096e42ba5e75ed9e
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2020-11-03 20:13:04 +0700

    ubah https jadi http

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ export default function App() {
   }, [])
   async function getListData() {
     const response = await fetch(
-      'https://103.123.229.45:4000/dashboard/sales-by-region'
+      'http://103.123.229.45:4000/dashboard/sales-by-region'
     )
     // console.log('haaaaaaa')
     const json = await response.json()
